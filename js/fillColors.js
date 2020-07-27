cb83f44af7f030537fc4e186b50c8636bfbb8565
Diogo Ribeiro <diogo.engsoftware@gmail.com>
2020-07-27 06:13:21 -0300

    Add color info to the color box

--- js/fillColors.js
+++ js/fillColors.js
@@ -1,7 +1,25 @@
+const copyIcon = `
+<svg viewBox="0 0 24 24">
+    <path fill="currentColor" d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z" />
+</svg>
+`;
+
+function createCorlorName(colorName) {
+  const colorNameBox = document.createElement('span');
+  colorNameBox.innerHTML = `
+    ${colorName}
+    ${copyIcon}
+  `;
+
+  return colorNameBox;
+}
+
 function updateBackgrounds(colors, colorBoxesContainer) {
   colors.forEach((color) => {
     const box = document.createElement('div');
-    box.style.backgroundColor = `rgb(${color.join(',')})`;
+    const colorString = `rgb(${color.join(',')})`;
+    box.style.backgroundColor = colorString;
+    box.appendChild(createCorlorName(colorString));
 
     colorBoxesContainer.appendChild(box);
   });
