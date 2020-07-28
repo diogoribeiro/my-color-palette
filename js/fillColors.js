97dba5eda83a1133138c31d3f73e2510e5282c29
Diogo Ribeiro <diogo.engsoftware@gmail.com>
2020-07-28 18:50:13 -0300

    Update color name styles

--- js/fillColors.js
+++ js/fillColors.js
@@ -1,15 +1,10 @@
-const copyIcon = `
-<svg viewBox="0 0 24 24">
-    <path fill="currentColor" d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z" />
-</svg>
-`;
+const WHITE_COLOR = '#FFF';
 
 function createCorlorName(colorName) {
   const colorNameBox = document.createElement('span');
-  colorNameBox.innerHTML = `
-    ${colorName}
-    ${copyIcon}
-  `;
+  const className = tinycolor.isReadable(WHITE_COLOR, colorName) ? '' : 'dark';
+  colorNameBox.className = className;
+  colorNameBox.innerText = colorName;
 
   return colorNameBox;
 }
@@ -17,7 +12,7 @@ function createCorlorName(colorName) {
 function updateBackgrounds(colors, colorBoxesContainer) {
   colors.forEach((color) => {
     const box = document.createElement('div');
-    const colorString = `rgb(${color.join(',')})`;
+    const colorString = tinycolor(`rgb(${color.join(',')})`).toHexString();
     box.style.backgroundColor = colorString;
     box.appendChild(createCorlorName(colorString));
 
